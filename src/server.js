e83707d0dc0cdc1e9f6c9d0e6caa77117e94998e
Lucas Baron <lucasbaron28@hotmail.com>
2021-03-31 11:47:53 -0300

    Instalando nunjucks

--- src/server.js
+++ src/server.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const server = express()
 
+
+//Configurar pasta pública
+server.use(express.static('public'))
+
+//Utilizando template engine
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
 //configurar caminhos da aplicação
 //página iniciali
 //req: Requisição
 //res: Resposta
 server.get('/', (req, res) => {
-     res.send('index')
+     return res.render('index.html' )
+})
+
+//Criando rota da pasta create-point
+server.get('/create-point', (req, res) => {
+    return res.render('create-point.html' )
+})
+
+//Criando rota para searsh-results
+server.get('/search-results', (req, res) => {
+    return res.render('search-results.html' )
 })
 
 //ligar servidor
